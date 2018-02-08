2b0ba54b17f9e0077559bfb8b1d9f43457a48dc2
Dvorkin <dpavel20@gmail.com>
2018-02-08 09:46:05 -0500

    Abstract classes 13

---
+++ 30daysOfCode/AbstractClasses13.py
@@ -0,0 +1,12 @@
+#https://www.hackerrank.com/challenges/30-abstract-classes/problem
+
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        Book.__init__(self, title, author)
+        self.price = price
+    def display(self):
+        print "Title:", self.title
+        print "Author:",self.author
+        print "Price:",self.price
